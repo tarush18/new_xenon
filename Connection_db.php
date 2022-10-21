886192fe2a07af4b1d80d8e43cdbf5321ee48073
tarush18 <93340107+tarush18@users.noreply.github.com>
2022-10-21 13:31:03 +0530

    Update Connection_db.php

--- Connection_db.php
+++ Connection_db.php
@@ -1,9 +1,14 @@
 <?php
 error_reporting(0);
-	$servername = "ec2-44-210-228-110.compute-1.amazonaws.com";
-	$username = "dpekwoiqzanyaq";
-	$password ="2c2817110da54a26c7875bf475083fccca555f2eab41e5bf3bf69ffec9e19f6b";
-	$dbname ="d12v0us8kedq7j";
+// 	$servername = "ec2-44-210-228-110.compute-1.amazonaws.com";
+// 	$username = "dpekwoiqzanyaq";
+// 	$password ="2c2817110da54a26c7875bf475083fccca555f2eab41e5bf3bf69ffec9e19f6b";
+// 	$dbname ="d12v0us8kedq7j";
+
+        $servername = "remotemysql.com";
+	$username = "JE75CU9Fcy";
+	$password ="55ZyN8weDb";
+	$dbname ="JE75CU9Fcy";
 
 	$check_connect=mysqli_connect($servername , $username , $password , $dbname);
 
